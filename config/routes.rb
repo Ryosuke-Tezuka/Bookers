026f4b0eb88fc247e6da53e8b206d11b37f12bf7
Ryosuke Tezuka <nanairo@RyosukenoMacBook-Pro.local>
2019-05-13 14:00:07 +0900

    [Update] 機能面の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get '/' => 'root#top'
+
+  get 'posts' => 'posts#index'
+  post 'posts/create' => 'posts#create'
+  get 'posts/:id/edit' => 'posts#edit', as: 'edit_post'
+  patch 'posts/:id' => 'posts#update', as: 'update_post'
+  delete 'posts/:id' => 'posts#destroy', as: 'destroy_post'
+  get 'posts/:id' => 'posts#show', as: 'post'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
